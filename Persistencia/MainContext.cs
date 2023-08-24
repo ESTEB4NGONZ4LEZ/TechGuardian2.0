773afdb2d9f805f25300701111dcbc888a953b8f
ESTEB4NGONZ4LEZ <kyosamacps@gmail.com>
2023-08-24 18:53:33 -0500

    :sparkles: feat: DbSet de las entidades

--- Persistencia/MainContext.cs
+++ Persistencia/MainContext.cs
@@ -10,6 +10,26 @@ public class MainContext : DbContext
     public MainContext(DbContextOptions<MainContext> options) : base(options)
     {
     }
+    public DbSet<Area> Areas { get; set; }
+    public DbSet<Arl> Arl { get; set; }
+    public DbSet<Categoria> Categorias { get; set; }
+    public DbSet<Ciudad> Ciudades { get; set; }
+    public DbSet<CompoCompu> CompoCompus { get; set; }
+    public DbSet<Componente> Componentes { get; set; }
+    public DbSet<Computador> Computadores { get; set; }
+    public DbSet<Departamento> Departamentos { get; set; }
+    public DbSet<Direccion> Direcciones { get; set; }
+    public DbSet<Eps> Eps { get; set; }
+    public DbSet<Estado> Estados { get; set; }
+    public DbSet<Insidencia> Insidencias { get; set; }
+    public DbSet<Lugar> Lugares { get; set; }
+    public DbSet<Pais> Paises { get; set; }
+    public DbSet<Persona> Personas { get; set; }
+    public DbSet<Rol> Roles { get; set; }
+    public DbSet<TipoDocumento> TipoDocumentos { get; set; }
+    public DbSet<TipoEmail> TipoEmails { get; set; }
+    public DbSet<TipoInsidencia> TipoInsidencias { get; set; }
+    public DbSet<TipoTelefono> TipoTelefonos { get; set; }
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<CompoCompu>().HasKey(x => new { x.Id_computador, x.Id_tipo_componente});
