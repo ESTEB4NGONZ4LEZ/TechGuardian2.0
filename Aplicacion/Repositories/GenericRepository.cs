1c6960f1d10ba4db3a8426c86e3a37e4ee47f40d
ESTEB4NGONZ4LEZ <kyosamacps@gmail.com>
2023-08-24 20:59:18 -0500

    :recycle: feat: Repositorio Generico

---
+++ Aplicacion/Repositories/GenericRepository.cs
@@ -0,0 +1,49 @@
+
+using System.Linq.Expressions;
+using Dominio.Base;
+using Dominio.Interface;
+using Microsoft.EntityFrameworkCore;
+using Persistencia;
+
+namespace Aplicacion.Repositories;
+
+public class GenericRepository<T> : IGeneric<T> where T : BaseEntity
+{
+    private readonly MainContext _context;
+    public GenericRepository(MainContext context)
+    {
+        _context = context;
+    }
+    public virtual async Task<IEnumerable<T>> GetAllAsync()
+    {
+        return await _context.Set<T>().ToListAsync();
+    }
+    public virtual async Task<T> GetByIdAsync(int id)
+    {
+        return await _context.Set<T>().FindAsync(id);
+    }
+    public virtual void Add(T entity)
+    {
+        _context.Set<T>().Add(entity);
+    }
+    public virtual void AddRange(IEnumerable<T> entities)
+    {
+        _context.Set<T>().AddRange(entities);
+    }
+    public virtual void Update(T entity)
+    {
+        _context.Set<T>().Update(entity);
+    }
+    public virtual void Remove(T entity)
+    {
+        _context.Set<T>().Remove(entity);
+    }
+    public virtual void RemoveRange(IEnumerable<T> entities)
+    {
+        _context.Set<T>().RemoveRange(entities);
+    }
+    public virtual IEnumerable<T> Find(Expression<Func<T, bool>> expression)
+    {
+        return _context.Set<T>().Where(expression);
+    }
+}
