4a1517df26b57e58343e8374cc24d56e490d852b
ESTEB4NGONZ4LEZ <kyosamacps@gmail.com>
2023-08-24 20:31:48 -0500

    :recycle: feat: Interfaz generica

---
+++ Dominio/Interface/IGeneric.cs
@@ -0,0 +1,17 @@
+
+using System.Linq.Expressions;
+using Dominio.Base;
+
+namespace Dominio.Interface;
+
+public interface IGeneric<T> where T : BaseEntity
+{ 
+    Task<IEnumerable<T>> GetAllAsync();
+    Task<T> GetByIdAsync(int id); 
+    void Add(T entity);
+    void AddRange(IEnumerable<T> entities);
+    void Remove(T entity);
+    void RemoveRange(IEnumerable<T> entities);
+    void Update(T entity);
+    IEnumerable<T> Find(Expression<Func<T, bool>> expression);
+}
